48d18b66cabc80b8f6a41fff02f3ffebce3f8d0d
Roberto Casadei <roby.casadei@unibo.it>
2022-11-09 14:51:04 +0100

    Wildcard code

--- src/main/java/it/unibo/generics/wildcard/Wildcard.java
+++ src/main/java/it/unibo/generics/wildcard/Wildcard.java
@@ -4,24 +4,23 @@ import it.unibo.generics.iterators.*;
 
 public class Wildcard {
 	// Metodo che usa la wildcard
-    public static void printAll(Iterator<?> it){
-    	while (it.hasNext()){
-    		System.out.println(it.next());
-    	}
-    }
-    
-	// Analoga versione con metodo generico    
-    public static <T> void printAll2(Iterator<T> it){
-    	while (it.hasNext()){
-    		System.out.println(it.next());
-    	}
-    }
-    
-    // Quale versione preferibile?
-    
-    public static void main(String[] s){
-    	Wildcard.printAll(new IntRangeIterator(1,5));
-    	Wildcard.printAll2(new IntRangeIterator(1,5));
-    	Wildcard.<Integer>printAll2(new IntRangeIterator(1,5));
-    }
-} 
+	public static void printAll(Iterator<?> it) {
+		while (it.hasNext()) {
+			System.out.println(it.next());
+		}
+	}
+
+	// Analoga versione con metodo generico
+	public static <T> void printAll2(Iterator<T> it) {
+		while (it.hasNext()) {
+			System.out.println(it.next());
+		}
+	}
+
+	// Quale versione preferibile?
+	public static void main(String[] s) {
+		Wildcard.printAll(new IntRangeIterator(1, 5));
+		Wildcard.printAll2(new IntRangeIterator(1, 5));
+		Wildcard.<Integer>printAll2(new IntRangeIterator(1, 5));
+	}
+}
---
+++ src/main/java/it/unibo/generics/wildcard/Wildcard2.java
@@ -0,0 +1,25 @@
+package it.unibo.generics.wildcard;
+
+import it.unibo.generics.generics.Vector;
+
+public class Wildcard2 {
+	// Metodo che usa la wildcard
+	public static Vector<Integer> toIntVector(Vector<? extends Number> vec) {
+		Vector<Integer> out = new Vector<>();
+		for(int i = 0; i < vec.getLength(); i++) {
+			// Si noti accesso al metodo intValue() del contratto di Number
+			out.addElement(vec.getElementAt(i).intValue());
+		}
+		return out;
+	}
+
+	// Quale versione preferibile?
+	public static void main(String[] s) {
+		Vector<Double> vd = new Vector<>();
+		vd.addElement(1.5);
+		vd.addElement(6.7);
+		Vector<Integer> vi = toIntVector(vd);
+		System.out.println("" + vi.getElementAt(0) + ", " + vi.getElementAt(1));
+		// var vd2 = toIntVector(new Vector<String>()); // ERROR: method not applicable
+	}
+}
