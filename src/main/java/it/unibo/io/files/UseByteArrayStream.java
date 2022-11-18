e12107188938f52525eb0225288a2028fe99dbb0
Danilo Pianini <danilo.pianini@unibo.it>
2022-11-18 17:17:22 +0100

    fix indentation

--- src/main/java/it/unibo/io/files/UseByteArrayStream.java
+++ src/main/java/it/unibo/io/files/UseByteArrayStream.java
@@ -5,19 +5,19 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class UseByteArrayStream {
-	public static void main(String[] args) throws IOException {
-		final byte[] b = new byte[] { 10, 20, -1, 40, -58 };
-		final InputStream in = new ByteArrayInputStream(b);
-		try {
-			int c = in.read();
-			while (c != -1) { // C-style
-				System.out.println(c);
-				c = in.read();
-			}
-			System.out.println("End of stream");
-		} finally { // assicura la chiusura anche con eccezioni
-			in.close(); // problema: può tirare eccezione!
-			// Notare che il main può lanciare IOException, molto scomodo!
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        final byte[] b = new byte[] { 10, 20, -1, 40, -58 };
+        final InputStream in = new ByteArrayInputStream(b);
+        try {
+            int c = in.read();
+            while (c != -1) {
+                System.out.println(c);
+                c = in.read();
+            }
+            System.out.println("End of stream");
+        } finally { // assicura la chiusura anche con eccezioni
+            in.close(); // problema: può tirare eccezione!
+            // Notare che il main può lanciare IOException, molto scomodo!
+        }
+    }
 }
