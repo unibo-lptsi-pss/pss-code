8e0e2dd9d912a37f32b847c11a49783dab54d825
Roberto Casadei <robyonrails@gmail.com>
2022-11-23 11:50:19 +0100

    Format code on lambdas

--- src/main/java/it/unibo/lambdas/first/RunnableUtility.java
+++ src/main/java/it/unibo/lambdas/first/RunnableUtility.java
@@ -3,26 +3,25 @@ package it.unibo.lambdas.first;
 import java.util.*;
 
 public class RunnableUtility {
-	private  static void iterate(final int howMany, final Runnable r){
-		for (int i = 0; i < howMany; i++){
+	private static void iterate(final int howMany, final Runnable r) {
+		for (int i = 0; i < howMany; i++) {
 			r.run();
 		}
 	}
-	
-	private static void batchExecution(final List<Runnable> list){
-		for (final Runnable r: list){
+
+	private static void batchExecution(final List<Runnable> list) {
+		for (final Runnable r : list) {
 			r.run();
 		}
 	}
 
 	public static void main(String[] args) {
-		iterate(10,()-> System.out.println("ok"));
-		final List<Runnable> list=Arrays.asList( 
-				()->System.out.println("a"),
-				()->System.out.println("b"),
-				()->System.out.println("c"),
-				()->System.exit(0)
-		); // Inferenza su asList automatica!
+		iterate(10, () -> System.out.println("ok"));
+		final List<Runnable> list = Arrays.asList(
+				() -> System.out.println("a"),
+				() -> System.out.println("b"),
+				() -> System.out.println("c"),
+				() -> System.exit(0)); // Inferenza su asList automatica!
 		batchExecution(list);
 	}
 }
