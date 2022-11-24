2bd433a60026bf4cbb0df98176ad72b69c64d493
Roberto Casadei <robyonrails@gmail.com>
2022-11-24 10:02:00 +0100

    Format stream example

--- src/main/java/it/unibo/streams/UseTransformations.java
+++ src/main/java/it/unibo/streams/UseTransformations.java
@@ -4,27 +4,26 @@ import java.util.*;
 
 public class UseTransformations {
 	public static void main(String[] args) {
-		final List<Integer> li = List.of(10,20,30,5,6,7,10,20,100);
-		
+		final List<Integer> li = List.of(10, 20, 30, 5, 6, 7, 10, 20, 100);
+
 		System.out.print("All\t\t :");
-		li.stream()
-		  .forEach(i->System.out.print(" "+i));
-		
+		li.stream().forEach(i -> System.out.print(" " + i));
+
 		System.out.print("\nFilter(>10)\t :");
 		li.stream()
-		  .filter(i->i>10) // fa passare solo certi elementi
-		  .forEach(i->System.out.print(" "+i));
-		
+				.filter(i -> i > 10) // fa passare solo certi elementi
+				.forEach(i -> System.out.print(" " + i));
+
 		System.out.print("\nMap(N:i+1)\t :");
-		li.stream()			
-		  .map(i->"N:"+(i+1)) // trasforma ogni elemento
-		  .forEach(i->System.out.print(" "+i));
-		
+		li.stream()
+				.map(i -> "N:" + (i + 1)) // trasforma ogni elemento
+				.forEach(i -> System.out.print(" " + i));
+
 		System.out.print("\nflatMap(i,i+1)\t :");
 		li.stream()
-		  .flatMap(i->List.of(i,i+1).stream()) // trasforma e appiattisce
-		  .map(String::valueOf) // invece del forEach.. 
-		  .map(" "::concat)
-		  .forEach(System.out::print);
+				.flatMap(i -> List.of(i, i + 1).stream()) // trasforma e appiattisce
+				.map(String::valueOf) // invece del forEach..
+				.map(" "::concat)
+				.forEach(System.out::print);
 	}
 }
