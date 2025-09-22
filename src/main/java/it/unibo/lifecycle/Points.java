9ccf20e2572a51cc26a724231b7079afc45de9da
Danilo Pianini <danilo.pianini@unibo.it>
2025-09-22 18:46:57 +0200

    Refactor Points.java: rename zero to ZERO for consistency

--- src/main/java/it/unibo/lifecycle/Points.java
+++ src/main/java/it/unibo/lifecycle/Points.java
@@ -1,16 +1,16 @@
 package it.unibo.lifecycle;
 
 class Points { // Modulo con funzionalità per punti 
-	
-	static Point3D zero = new Point3D().build(0, 0, 0);
 
-	static Point3D max(Point3D[] ps) { // metodo statico
-		Point3D max = zero; // ricerca max
-		for (Point3D elem : ps) {
-			if (elem.getModule() > max.getModule()) {
-				max = elem;
-			}
-		}
-		return max;
-	}
+    static Point3D ZERO = new Point3D().build(0, 0, 0);
+
+    static Point3D max(Point3D[] ps) { // metodo statico
+        Point3D max = zero; // ricerca max
+        for (Point3D elem : ps) {
+            if (elem.getModule() > max.getModule()) {
+                max = elem;
+            }
+        }
+        return max;
+    }
 }
