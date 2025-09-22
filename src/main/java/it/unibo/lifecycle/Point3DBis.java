95243657e31037fe39589be1afa980c6b574bd68
Danilo Pianini <danilo.pianini@unibo.it>
2025-09-22 18:44:14 +0200

    refactor(Point3DBis): improve code formatting for consistency and readability

--- src/main/java/it/unibo/lifecycle/Point3DBis.java
+++ src/main/java/it/unibo/lifecycle/Point3DBis.java
@@ -1,30 +1,30 @@
 package it.unibo.lifecycle;
 
 class Point3DBis { // dichiarazione classe
-	double x; // 3 campi
-	double y;
-	double z;
+    double x; // 3 campi
+    double y;
+    double z;
 
-	Point3DBis build(double a, double b, double c) { // build con ritorno
-		this.x = a;
-		this.y = b;
-		this.z = c;
-		return this;
-	}
+    Point3DBis build(double a, double b, double c) { // build con ritorno
+        this.x = a;
+        this.y = b;
+        this.z = c;
+        return this;
+    }
 
-	double getModule() { 
-		return this.x * this.x + this.y * this.y + this.z * this.z;
-	}
+    double getModule() {
+        return this.x * this.x + this.y * this.y + this.z * this.z;
+    }
 
-	static Point3DBis ZERO = new Point3DBis().build(0, 0, 0);
+    static Point3DBis ZERO = new Point3DBis().build(0, 0, 0);
 
-	static Point3DBis max(Point3DBis[] ps) { // metodo statico
-		Point3DBis max = ZERO; // ricerca max
-		for (Point3DBis elem : ps) {
-			if (elem.getModule() > max.getModule()) {
-				max = elem;
-			}
-		}
-		return max;
-	}
+    static Point3DBis max(Point3DBis[] ps) { // metodo statico
+        Point3DBis max = ZERO; // ricerca max
+        for (Point3DBis elem : ps) {
+            if (elem.getModule() > max.getModule()) {
+                max = elem;
+            }
+        }
+        return max;
+    }
 }
--- src/main/java/it/unibo/lifecycle/UsePoint3DBis.java
+++ src/main/java/it/unibo/lifecycle/UsePoint3DBis.java
@@ -1,20 +1,20 @@
 package it.unibo.lifecycle;
 
 class UsePoint3DBis {
-	public static void main(String[] s) {
-		// creo vari punti
-		Point3DBis p1 = new Point3DBis().build(10, 20, 30);
-		Point3DBis p2 = new Point3DBis().build(5, 6, 7);
-		Point3DBis p3 = new Point3DBis().build(100, 100, 100);
-		Point3DBis p4 = Point3DBis.ZERO; // questo è lo zero
+    static void main(String[] s) {
+        // creo vari punti
+        Point3DBis p1 = new Point3DBis().build(10, 20, 30);
+        Point3DBis p2 = new Point3DBis().build(5, 6, 7);
+        Point3DBis p3 = new Point3DBis().build(100, 100, 100);
+        Point3DBis p4 = Point3DBis.ZERO; // questo è lo zero
 
-		// costruisco l'array
-		Point3DBis[] array = new Point3DBis[] { p1, p2, p3, p4 };
+        // costruisco l'array
+        Point3DBis[] array = new Point3DBis[]{p1, p2, p3, p4};
 
-		// calcolo il max
-		Point3DBis max = Point3DBis.max(array);
+        // calcolo il max
+        Point3DBis max = Point3DBis.max(array);
 
-		// stampo
-		System.out.println("Max: " + max.x + "," + max.y + "," + max.z);
-	}
+        // stampo
+        System.out.println("Max: " + max.x + "," + max.y + "," + max.z);
+    }
 }
