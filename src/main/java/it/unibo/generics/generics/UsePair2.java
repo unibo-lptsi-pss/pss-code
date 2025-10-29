c5f93674c846ae4c72af20433d4228d54b861553
Danilo Pianini <danilo.pianini@unibo.it>
2025-10-29 15:54:56 +0100

    Fix the damn indentation

--- src/main/java/it/unibo/generics/generics/UsePair2.java
+++ src/main/java/it/unibo/generics/generics/UsePair2.java
@@ -2,16 +2,14 @@ package it.unibo.generics.generics;
 
 public class UsePair2 {
     public static void main(String[] s) {
-    	// Parametri in new Vector() inferiti dal tipo della variabile  	
-    	final Vector<Pair<String,Integer>> v = new Vector<>(); 
-    	// Parametri in new Pair(..) inferiti dal tipo degli argomenti
-    	v.addElement(new Pair<>("Prova",1));		
-    	v.addElement(new Pair<>("Vettore",2));
-		final String str = v.getElementAt(0).getFirst() + " " +
-			v.getElementAt(1).getFirst(); // Nota, nessun cast!
-		System.out.println(str);
-		System.out.println(v);
-    
+        // Parametri in new Vector() inferiti dal tipo della variabile
+        final Vector<Pair<String, Integer>> v = new Vector<>();
+        // Parametri in new Pair(..) inferiti dal tipo degli argomenti
+        v.addElement(new Pair<>("Prova", 1));
+        v.addElement(new Pair<>("Vettore", 2));
+        final int v1second = v.getElementAt(1).getSecond(); // Nota, nessun cast!
+        final String str = v.getElementAt(0).getFirst() + " " + v1second;
+        System.out.println(str);
+        System.out.println(v);
     }
 }
-
