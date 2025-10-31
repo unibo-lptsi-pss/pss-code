0a07cecf3a0b3a59ef22aef3d4bccc2fd982023e
Danilo Pianini <danilo.pianini@unibo.it>
2025-10-31 13:40:09 +0100

    Add final modifier to parameters in addElementToVector and returnFirstNumber

--- src/main/java/it/unibo/generics/wildcard/Wildcard3.java
+++ src/main/java/it/unibo/generics/wildcard/Wildcard3.java
@@ -4,14 +4,14 @@ import it.unibo.generics.generics.Vector;
 
 public class Wildcard3 {
     // Le wildcard pongono limiti sulle operazioni eseguibili
-    public static void addElementToVector(Vector<? extends Number> vec) {
+    public static void addElementToVector(final Vector<? extends Number> vec) {
         // Per Vector<T> covariante non possiamo invocare metodi che accettano T in input
         final Number n = vec.getElementAt(0); // questo è ok
         Number toAdd = n.doubleValue() + 1;
         // vec.addElement(toAdd); // ERROR!
     }
 
-    public static Number returnFirstNumber(Vector<? super Integer> vec) {
+    public static Number returnFirstNumber(final Vector<? super Integer> vec) {
         // Per Vector<T> contravariante non possiamo invocare metodi che restituiscono T in output
         vec.addElement(Integer.valueOf(7)); // questo è ok
         Number out = null;
