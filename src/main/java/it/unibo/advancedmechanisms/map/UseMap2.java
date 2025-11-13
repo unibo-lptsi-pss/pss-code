f370be519ca12303415520ce00ef6882aa550021
Danilo Pianini <danilo.pianini@unibo.it>
2025-11-13 11:50:37 +0100

    Refactor UseMap2 to use LinkedHashMap and improve output formatting

--- src/main/java/it/unibo/advancedmechanisms/map/UseMap2.java
+++ src/main/java/it/unibo/advancedmechanisms/map/UseMap2.java
@@ -1,23 +1,22 @@
 package it.unibo.advancedmechanisms.map;
 
-import java.util.*;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
-public class UseMap2 {	
-	public static void main(String[] args) {
-		// Al solito, uso una incarnazione, ma poi lavoro sull'interfaccia
-		final Map<Integer, String> map = new HashMap<>();
-		// Una mappa è una funzione discreta
-		map.put(345211, "Bianchi");
-		map.put(345122, "Rossi");
-		map.put(243001, "Verdi");
-
-		for (final Map.Entry<Integer, String> entry : map.entrySet()) {
-			System.out.println(entry.getClass());
-			System.out.println(entry.getKey());
-			System.out.println(entry.getValue());
-			entry.setValue(entry.getValue()+"...");
-		}
-		System.out.println(map);
-		// {345211=null, 243001=null, 345122=null}
-	}
+public class UseMap2 {
+    static void main() {
+        // Al solito, creo una classe specifica, ma dichiaro (e quindi lavoro) sull'interfaccia
+        final Map<Integer, String> map = new LinkedHashMap<>();
+        map.put(345211, "Bianchi");
+        map.put(345122, "Rossi");
+        map.put(243001, "Verdi");
+        for (final Map.Entry<Integer, String> entry : map.entrySet()) {
+            System.out.println(entry.getClass());
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+            entry.setValue(entry.getValue() + " modificata!");
+        }
+        System.out.println(map);
+        // {345211=Bianchi modificata!, 345122=Rossi modificata!, 243001=Verdi modificata!}
+    }
 }
