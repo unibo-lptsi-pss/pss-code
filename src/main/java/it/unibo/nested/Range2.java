951ec67f53b50656d47b88f0ae92fd0bb349cb85
Danilo Pianini <danilo.pianini@unibo.it>
2025-11-13 12:00:58 +0100

    Refactor Range2 class to improve iterator implementation and formatting

--- src/main/java/it/unibo/nested/Range2.java
+++ src/main/java/it/unibo/nested/Range2.java
@@ -1,33 +1,34 @@
 package it.unibo.nested;
 
+import java.util.Iterator;
+
 public class Range2 implements Iterable<Integer> {
-	private final int start;
-	private final int stop;
+    private final int start;
+    private final int stop;
 
-	public Range2(final int start, final int stop) {
-		this.start = start;
-		this.stop = stop;
-	}
+    public Range2(final int start, final int stop) {
+        this.start = start;
+        this.stop = stop;
+    }
 
-	public java.util.Iterator<Integer> iterator() {
-		return this.new Iterator();
-	}
+    public Iterator<Integer> iterator() {
+        return this.new RangeIterator();
+    }
 
-	private class Iterator implements java.util.Iterator<Integer> {
-		private int current;
+    private class RangeIterator implements Iterator<Integer> {
+        private int current;
 
-		public Iterator() {
-			this.current = Range2.this.start; // this.current = start
-		}
+        public RangeIterator() {
+            this.current = Range2.this.start; // this.current = start
+        }
 
-		public Integer next() {
-			return this.current++;
-		}
+        public Integer next() { return this.current++; }
 
-		public boolean hasNext() {
-			return this.current <= Range2.this.stop;
-		}
+        public boolean hasNext() {
+            // Accesso diretto ai campi non-statici della classe esterna!
+            return this.current <= Range2.this.stop; // Equivalente: return current <= stop;
+        }
 
-		public void remove() { }
-	}
+        public void remove() { throw new UnsupportedOperationException(); }
+    }
 }
