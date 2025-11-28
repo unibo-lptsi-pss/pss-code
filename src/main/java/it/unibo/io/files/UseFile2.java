59a8c5a7a944fb9438c93670ebd0d9c92bd269ba
Danilo Pianini <danilo.pianini@unibo.it>
2025-11-28 11:11:39 +0100

    enrich UseFile2 and remove references to @mviroli's file systems

--- src/main/java/it/unibo/io/files/UseFile2.java
+++ src/main/java/it/unibo/io/files/UseFile2.java
@@ -1,12 +1,29 @@
 package it.unibo.io.files;
 
-import java.io.*;
+import java.io.File;
 
-public class UseFile2 {	
-	public static final String FILE_NAME = "/home/mirko/aula/oop/15/prova.bin";
+public class UseFile2 {
+    // "user.dir" -- da dove avete lanciato java
+    // "java.home" -- installazione java
+    // "file.separator" -- / su unix e \ su windows
+	public static final String USER_HOME = System.getProperty("user.home");
 
-	public static void main(String[] args) throws Exception {
-		final File f = new File( FILE_NAME );
-		System.out.println(f.getCanonicalPath());
+	static void main() throws Exception {
+        System.out.println("My home is at " + USER_HOME);
+		final File homeFolder = new File(USER_HOME);
+        System.out.println("Does my home exist? " + homeFolder.exists());
+        System.out.println("Is my home a directory? " + homeFolder.isDirectory());
+        System.out.println("Can I write on my home directory? " + homeFolder.canWrite());
+        final File myFolderToCreate = new File(homeFolder, "deleteme-pss-folder" + File.separator + "myfolder");
+        myFolderToCreate.deleteOnExit();
+        if (myFolderToCreate.exists()) {
+            throw new IllegalStateException("The folder exists");
+        }
+        if (!myFolderToCreate.mkdir()) {
+            System.out.println("Can't create the folder " +  myFolderToCreate + " using mkdirs...");
+            if (!myFolderToCreate.mkdirs()) {
+                throw new IllegalStateException();
+            }
+        }
 	}
 }
