c2897647d23e0762742b29d2200224092b949d01
Gianluca Aguzzi <gianluca.aguzzi@unibo.it>
2026-09-23 15:46:02 +0200

    update simple array

--- src/main/java/it/unibo/structured/SimpleArray.java
+++ src/main/java/it/unibo/structured/SimpleArray.java
@@ -1,7 +1,7 @@
 void main() {
     String[] words = {"Hello", "World!"};
     for(int i = 0; i < words.length; i++) {
-        System.out.print(words[i] + " ");
+        IO.print(words[i] + " ");
     }
     IO.println();
 }
